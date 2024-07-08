90da33b29c7c89049b27e13b7979d8d6e46a6118
Uday Sri Vishnu <129844163+uday-sv@users.noreply.github.com>
2024-07-08 11:39:54 +0530

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
-import numpy as npimport
+import numpy as np
 import pandas as pd
 import pickle
+import os  
 
 app = Flask(__name__)
 
@@ -18,19 +19,19 @@ def pred():
 @app.route('/predict', methods=['POST'])
 def predict():
     col = ['muscle_pain', 'nausea', 'throat_irritation', 'weight_loss',
-       'passage_of_gases', 'skin_peeling', 'blister', 'high_fever',
-       'swelling_of_stomach', 'fast_heart_rate', 'muscle_weakness', 'fatigue',
-       'red_spots_over_body', 'movement_stiffness', 'malaise', 'headache',
-       'yellowing_of_eyes', 'abdominal_pain', 'mild_fever', 'depression',
-       'knee_pain', 'swelled_lymph_nodes', 'loss_of_appetite', 'phlegm',
-       'blood_in_sputum', 'irritability', 'itching', 'spinning_movements',
-       'irregular_sugar_level', 'weight_gain', 'dark_urine',
-       'acute_liver_failure', 'lethargy', 'dischromic _patches',
-       'excessive_hunger', 'back_pain', 'obesity', 'swelling_joints',
-       'loss_of_balance', 'weakness_of_one_body_side', 'neck_pain',
-       'joint_pain', 'lack_of_concentration', 'indigestion',
-       'toxic_look_(typhos)', 'chills', 'pain_behind_the_eyes', 'sweating',
-       'constipation', 'restlessness']
+           'passage_of_gases', 'skin_peeling', 'blister', 'high_fever',
+           'swelling_of_stomach', 'fast_heart_rate', 'muscle_weakness', 'fatigue',
+           'red_spots_over_body', 'movement_stiffness', 'malaise', 'headache',
+           'yellowing_of_eyes', 'abdominal_pain', 'mild_fever', 'depression',
+           'knee_pain', 'swelled_lymph_nodes', 'loss_of_appetite', 'phlegm',
+           'blood_in_sputum', 'irritability', 'itching', 'spinning_movements',
+           'irregular_sugar_level', 'weight_gain', 'dark_urine',
+           'acute_liver_failure', 'lethargy', 'dischromic _patches',
+           'excessive_hunger', 'back_pain', 'obesity', 'swelling_joints',
+           'loss_of_balance', 'weakness_of_one_body_side', 'neck_pain',
+           'joint_pain', 'lack_of_concentration', 'indigestion',
+           'toxic_look_(typhos)', 'chills', 'pain_behind_the_eyes', 'sweating',
+           'constipation', 'restlessness']
 
     if request.method == 'POST':
         inputt = [str(x) for x in request.form.values()]
@@ -44,4 +45,5 @@ def predict():
         return render_template('results.html', predictedValue=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True) 
